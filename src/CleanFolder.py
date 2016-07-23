ac38a85c4635feb4073b5efec2705424f09bcf56
jkpark <sanaigon@gmail.com>
2016-07-23 09:39:23 +0900

    [CleanFolder][jkpark]
    [수정내용]
      - 새로운 코드로 수정

--- src/CleanFolder.py
+++ src/CleanFolder.py
@@ -1,28 +1,29 @@
 #-*- coding: utf-8 -*-
 import os
-from os.path import join
-import shutil
 import sys
+import shutil
 
 reload(sys)
-sys.setdefaultencoding(sys.getfilesystemencoding())
-targetDir = unicode(os.getcwd())
+sys.setdefaultencoding('cp949')
 
-def movefile(dirname, filelist):
-    for file in filelist:
-        print "MoveFile <" + join(dirname,file) + ">" + "--> <" + join(targetDir,file) +">"
-        shutil.move(join(dirname,file), join(targetDir,file))
-    shutil.rmtree(dirname)
+video_extension = [".avi", ".wmv", ".mp4", ".mov"]
 
-def main():
-    for root, dirs, files in os.walk(targetDir):
-        if targetDir == root:
-            continue
-        movefile(root, files)
+def get_file(target):
+    target_folder = "./" + target
+    exist_video = False
+    for root, dirs, files in os.walk(target_folder):
+        for file in files:
+            for ext in video_extension:
+                if os.path.splitext(file)[1] == ext:
+                    exist_video = True
+                    shutil.move(target_folder+"/"+ file.decode('cp949'), "./")
 
-if __name__ == "__main__":
-    input = raw_input(u"현재 실행하려는 경로가 " + unicode(os.getcwd()) + u"맞습니까?(Y/N)" )
-    if input == 'y' or input == 'Y':
-        main()
+    if exist_video == True:
+        shutil.rmtree(target_folder)
 
+def clean_file():
+    for root, dirs, files in os.walk('./'):
+        for dir in dirs:
+            get_file(dir)
 
+clean_file()
